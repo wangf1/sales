f78931edd200f73c62cb183447414912d695acdc
Wang, Feng <feng01.wang@sap.com>
2019-04-04 16:41:43 +0800

    Fix cannot sort by data issue.

--- management/src/main/webapp/sales/common/SortUtils.js
+++ management/src/main/webapp/sales/common/SortUtils.js
@@ -7,9 +7,12 @@ sales.common.SortUtils = (function() {
 
     function intAwareComparator(a, b) {
         var result;
-        var numA = parseFloat(a);
-        var numB = parseFloat(b);
+        // Use Number instead of parseFloat for strict parse,
+        // in order to make data string like 2019-01-01 not parse as 2019.
+        var numA = Number(a);
+        var numB = Number(b);
         if (!isNaN(numA) && !isNaN(numB)) {
+        	// Reason of sort by number is, string "100" should greater than "99"
             result = sap.ui.model.Sorter.defaultComparator(numA, numB);
         } else {
             result = sap.ui.model.Sorter.defaultComparator(a, b);
